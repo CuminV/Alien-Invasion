ef72993ec153d355f98e4cefd0d1b4895082c35c
BlackCumin <kostik.kocherga@gmail.com>
2026-03-19 09:17:10 +0200

    Main

---
+++ main/bullet.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """Class for control bullet`s"""
+    
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.image = pygame.image.load('Alien Invasion/images/bullet.bmp')
+        
+        self.rect = self.image.get_rect()
+        self.rect.midtop = ai_game.ship.rect.midtop
+        
+        self.y = float(self.rect.y)
+        
+    def update(self):
+        """Move bullet up to screen"""
+        
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
+        
+    def draw_bullet(self):
+        """Draw bullet to screen"""
+        self.screen.blit(self.image, self.rect)
+        
+    
---
+++ main/settings.py
@@ -0,0 +1,34 @@
+import pygame
+
+class Settings():
+    """Class for a settings Alien Invasion game`s"""
+    
+    def __init__(self):
+        #Screen settings
+        self.screen_width = 1600
+        self.screen_height = 800
+        self.background = pygame.image.load('Alien Invasion/images/starsky.bmp')
+        
+        #Ship settings
+        self.ship_speed = 1.5
+        
+        #Bullet settings
+        self.bullet = pygame.image.load('Alien Invasion/images/bullet.bmp')
+        self.bullet_speed = 2
+        self.bullet_wigth = 50
+        self.bullet_height = 50
+        self.bullet_color = (235, 9, 9)
+        self.bullets_allowed = 3
+        
+        #Alien settings
+        self.alien_speed = 0.75
+        self.fleet_drop_speed = 30
+        self.fleet_direction = 1
+        
+        
+        #sounds
+        self.sound_explosion = pygame.mixer.Sound('Alien Invasion/sound/explosion.wav')
+        self.sound_explosion.set_volume(0.5)
+        
+        self.sound_laser = pygame.mixer.Sound('Alien Invasion/sound/laser.wav')
+        self.sound_laser.set_volume(0.5)
